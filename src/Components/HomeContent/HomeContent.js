d1deebec5868cea967d83c6b2037f21d5b9d82a9
Boyd Duffie V <shmeeves@ufl.edu>
2020-08-11 14:21:13 -0400

    added home image background for the 4 themes that have designs. moving on to font sizes now

--- src/Components/HomeContent/HomeContent.js
+++ src/Components/HomeContent/HomeContent.js
@@ -1,37 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './HomeContent.css';
 import { useSpring, animated } from 'react-spring';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 export const HomeContent = (props) => {
+  const [hovering, setHover] = useState(false);
+  const welcomeSpring = useSpring({
+    transform: hovering ? 'scale(1.1)' : 'scale(1)',
+    textShadow: hovering
+      ? '0px 5px 5px rgba(0,0,0,0.5)'
+      : '0px 0px 0px rgba(0,0,0,0)',
+  });
+
   return (
-    <div className={`home-container ${props.theme}`} id="home">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div className="home-content">
-        <div className={`title-box ${props.theme}`}>
-          <h1 className={props.theme}>Boyd Duffie</h1>
-          <h2 className={props.theme}>
-            Computer Science • University of Florida • 2021
-          </h2>
-        </div>
-        <div
-          className="welcome"
-          onClick={() =>
-            window.scroll({
-              top: window.innerHeight,
-              left: 0,
-              behavior: 'smooth',
-            })
-          }>
-          <FontAwesomeIcon icon={faChevronDown} />
-          <p className={props.theme}>Welcome</p>
-        </div>
+    <div>
+      <div className={`home-background ${props.theme}`}>
+        <div className={`home-mascot bg-image ${props.theme}`}></div>
       </div>
-      <div></div>
+      <div className={`home-container ${props.theme}`} id="home">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="home-content">
+          <div className={`title-box ${props.theme}`}>
+            <h1 className={props.theme}>Boyd Duffie</h1>
+            <h2 className={props.theme}>
+              Computer Science • University of Florida • 2021
+            </h2>
+          </div>
+          <animated.div
+            className="welcome"
+            onClick={() =>
+              window.scroll({
+                top: window.innerHeight,
+                left: 0,
+                behavior: 'smooth',
+              })
+            }
+            style={{ ...welcomeSpring }}
+            onMouseOver={() => setHover(true)}
+            onMouseOut={() => setHover(false)}>
+            <p className={props.theme}>Welcome</p>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              style={{ ...{ fontSize: '2.5vw' } }}
+            />
+          </animated.div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -7,36 +7,6 @@ import { useSpring, animated } from 'react-spring';
 const themes = require('../../themes.json');
 
 export const Nav = (props) => {
-  const [homeHovering, setHomeHovering] = useState(false);
-  const homeSpring = useSpring({
-    color: homeHovering ? '#fff' : themes[props.theme]['link-color'],
-  });
-
-  const [aboutHovering, setAboutHovering] = useState(false);
-  const aboutSpring = useSpring({
-    color: aboutHovering ? '#fff' : themes[props.theme]['link-color'],
-  });
-
-  const [workHovering, setWorkHovering] = useState(false);
-  const workSpring = useSpring({
-    color: workHovering ? '#fff' : themes[props.theme]['link-color'],
-  });
-
-  const [contactHovering, setContactHovering] = useState(false);
-  const contactSpring = useSpring({
-    color: contactHovering ? '#fff' : themes[props.theme]['link-color'],
-  });
-
-  const [LIHovering, setLIHovering] = useState(false);
-  const LISpring = useSpring({
-    color: LIHovering ? '#fff' : themes[props.theme]['link-color'],
-  });
-
-  const [GHHovering, setGHHovering] = useState(false);
-  const GHSpring = useSpring({
-    color: GHHovering ? '#fff' : themes[props.theme]['link-color'],
-  });
-
   return (
     <div className={`navbar-grid ${props.theme}`}>
       <div className="mascots">
@@ -44,56 +14,31 @@ export const Nav = (props) => {
         <div className="mascot-2 image"></div>
       </div>
       <nav className={`nav-flex-container ${props.theme}`}>
-        <animated.a
-          className={`home ${props.theme}`}
-          href="#home"
-          style={{ ...homeSpring }}
-          onMouseOver={() => setHomeHovering(true)}
-          onMouseOut={() => setHomeHovering(false)}>
+        <a className={`home ${props.theme}`} href="#home">
           Home
-        </animated.a>
-        <animated.a
-          className={`about ${props.theme}`}
-          href="#about"
-          style={{ ...aboutSpring }}
-          onMouseOver={() => setAboutHovering(true)}
-          onMouseOut={() => setAboutHovering(false)}>
+        </a>
+        <a className={`about ${props.theme}`} href="#about">
           About
-        </animated.a>
-        <animated.a
-          className={`work ${props.theme}`}
-          href="#work"
-          style={{ ...workSpring }}
-          onMouseOver={() => setWorkHovering(true)}
-          onMouseOut={() => setWorkHovering(false)}>
+        </a>
+        <a className={`work ${props.theme}`} href="#work">
           Work
-        </animated.a>
-        <animated.a
-          className={`contact ${props.theme}`}
-          href="#contact"
-          style={{ ...contactSpring }}
-          onMouseOver={() => setContactHovering(true)}
-          onMouseOut={() => setContactHovering(false)}>
+        </a>
+        <a className={`contact ${props.theme}`} href="#contact">
           Contact
-        </animated.a>
-        <animated.a
+        </a>
+        &nbsp;
+        <a
           className="linkedin"
           href="https://www.linkedin.com/in/boydduffiev/"
-          target="_blank"
-          style={{ ...LISpring }}
-          onMouseOver={() => setLIHovering(true)}
-          onMouseOut={() => setLIHovering(false)}>
+          target="_blank">
           <FontAwesomeIcon icon={faLinkedin} />
-        </animated.a>
-        <animated.a
+        </a>
+        <a
           className="github"
           href="https://github.com/boydDuffie"
-          target="_blank"
-          style={{ ...GHSpring }}
-          onMouseOver={() => setGHHovering(true)}
-          onMouseOut={() => setGHHovering(false)}>
+          target="_blank">
           <FontAwesomeIcon icon={faGithub} />
-        </animated.a>
+        </a>
       </nav>
     </div>
   );
